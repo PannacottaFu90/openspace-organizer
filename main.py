bbfe1ea8a50e8b76cc34e7166a3554bf16edc537
pizzi <edoardo.pizzileo@gmail.com>
2026-02-13 10:24:14 +0100

    Lettura da file excel

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from utils.openspace import Openspace
+import pandas as pd
 
-names = [
+
+"""names = [
     "Mario",
     "Luigi",
     "Peach",
@@ -27,8 +29,16 @@ names = [
     "Nancy",
     "edo",
     "tommy",
-]
+]"""
 
+
+file_presenze = "presenze.xlsx"
+# Read names from the Excel file
+df = pd.read_excel(file_presenze, sheet_name="Sheet1")
+names_column = df["Nome"].dropna()
+names = names_column.tolist()
+
+# Initialize Openspace and organize the names
 ufficio = Openspace()
 ufficio.organize(names)
 ufficio.display()
