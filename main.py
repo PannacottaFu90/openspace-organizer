cd27f1106336c16d4884ded21e9219b432edfd9c
pizzi <edoardo.pizzileo@gmail.com>
2026-02-16 12:19:05 +0100

    migliorata UX

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from utils.openspace import Openspace
 import pandas as pd
+import tkinter as tk
+import os
+from tkinter import filedialog
 
 
 """names = [
@@ -31,12 +34,39 @@ import pandas as pd
     "tommy",
 ]"""
 
+# Nasconde la finestra principale di tkinter
+root = tk.Tk()
+root.withdraw()
 
-file_presenze = "presenze.xlsx"
-# Read names from the Excel file
-df = pd.read_excel(file_presenze, sheet_name="Sheet1")
-names_column = df["Nome"].dropna()
-names = names_column.tolist()
+print(
+    "Seleziona il file delle presenze dalla finestra che si è aperta.\nInput files: *.xlsx *.xls, *.csv  \nAssicurati che non ci siano intestazioni \n Se carichi un file excel, i nomi devono essere nella prima colonna"
+)
+path = filedialog.askopenfilename(
+    title="Seleziona File Presenze",
+    filetypes=[
+        ("File supportati", "*.xlsx *.xls, *.csv"),
+        ("Excel files", "*.xlsx *.xls"),
+        ("CSV files", "*.csv"),
+    ],
+)
+
+if path:
+    extension = os.path.splitext(path)[1].lower()
+
+    try:
+        if extension == ".csv":
+            df = pd.read_csv(path, header=None, sep=None, engine="python")
+        else:
+            df = pd.read_excel(path, header=None)
+
+        names_column = df.iloc[
+            :, 0
+        ].dropna()  # Prende la prima colonna e rimuove eventuali valori NaN
+        names = names_column.tolist()
+
+    except Exception as e:
+        print(f"Errore durante la lettura del file {extension}: {e}")
+        sys.exit()
 
 # Initialize Openspace and organize the names
 ufficio = Openspace()
